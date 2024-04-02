ed67fd3e4aa0076412ad1b973e2f4f3647632027
Aleksandr.Loginov <Aleksandr.Loginov@x5.ru>
2024-04-02 12:54:14 +0300

    Project init

---
+++ src/main/java/ru/x5/dnd/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.x5.dnd.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/x5/dnd/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.x5.dnd.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
