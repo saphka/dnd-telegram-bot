89bbf76c3c024d73045c75e170828ad569309da6
Aleksandr.Loginov <Aleksandr.Loginov@x5.ru>
2024-08-16 18:01:03 +0300

    feat: debounce greeting

---
+++ src/main/java/ru/x5/dnd/telegrambot/service/GreetMemberService.java
@@ -0,0 +1,67 @@
+package ru.x5.dnd.telegrambot.service;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.context.MessageSource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Service
+public class GreetMemberService {
+
+    public static final int GREETING_DELAY = 30000;
+    private final TelegramService telegramService;
+    private final MessageSource messageSource;
+    private final Map<Long, Set<String>> greetings = new ConcurrentHashMap<>();
+
+    public GreetMemberService(TelegramService telegramService, MessageSource messageSource) {
+        this.telegramService = telegramService;
+        this.messageSource = messageSource;
+    }
+
+    public void greet(Long chatId, List<User> users) {
+        List<String> newUserNames = users.stream().map(User::getUserName).toList();
+        greetings.compute(chatId, (chatIdKey, usersValue) -> {
+            if (usersValue == null) {
+                usersValue = new HashSet<>();
+            }
+            usersValue.addAll(newUserNames);
+            return usersValue;
+        });
+    }
+
+    @Scheduled(fixedDelay = GREETING_DELAY)
+    public void greetDelayed() {
+        for (Long chatId : greetings.keySet()) {
+            Set<String> users = greetings.remove(chatId);
+            if (CollectionUtils.isNotEmpty(users)) {
+                greetByChatId(chatId, users);
+            }
+        }
+    }
+
+    private void greetByChatId(Long chatId, Set<String> users) {
+        var msg = new SendMessage();
+        msg.setChatId(chatId);
+        msg.setText(messageSource.getMessage("greeting.new-member", new Object[]{formatMembers(users)}, Locale.getDefault()));
+        try {
+            telegramService.execute(msg);
+        } catch (TelegramApiException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private String formatMembers(Set<String> users) {
+        return users
+                .stream()
+                .map(mem -> "@" + mem)
+                .collect(Collectors.joining(", "));
+    }
+
+}
--- src/main/java/ru/x5/dnd/telegrambot/service/actions/GreetMembersAction.java
+++ src/main/java/ru/x5/dnd/telegrambot/service/actions/GreetMembersAction.java
@@ -3,44 +3,26 @@ package ru.x5.dnd.telegrambot.service.actions;
 import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.x5.dnd.telegrambot.config.StateMachineEvents;
 import ru.x5.dnd.telegrambot.config.StateMachineStates;
 import ru.x5.dnd.telegrambot.config.TelegramMessageHeaders;
-import ru.x5.dnd.telegrambot.service.TelegramService;
-
-import java.text.MessageFormat;
-import java.util.stream.Collectors;
+import ru.x5.dnd.telegrambot.service.GreetMemberService;
 
 @Service
 public class GreetMembersAction implements Action<StateMachineStates, StateMachineEvents> {
 
-    private final TelegramService telegramService;
+    private final GreetMemberService greetMemberService;
 
-    public GreetMembersAction(TelegramService telegramService) {
-        this.telegramService = telegramService;
+    public GreetMembersAction(GreetMemberService greetMemberService) {
+        this.greetMemberService = greetMemberService;
     }
 
     @Override
     public void execute(StateContext<StateMachineStates, StateMachineEvents> context) {
         var message = (Message) context.getMessageHeader(TelegramMessageHeaders.MESSAGE);
-        var msg = new SendMessage();
-        msg.setChatId(message.getChatId());
-        msg.setText(MessageFormat.format("Добро пожаловать, {0}!", formatMembers(message)));
-        try {
-            telegramService.execute(msg);
-        } catch (TelegramApiException e) {
-            throw new IllegalStateException(e);
-        }
+        greetMemberService.greet(message.getChatId(), message.getNewChatMembers());
     }
 
-    private String formatMembers(Message message) {
-        return message
-                .getNewChatMembers()
-                .stream()
-                .map(mem -> "@" + mem.getUserName())
-                .collect(Collectors.joining(", "));
-    }
+
 }
